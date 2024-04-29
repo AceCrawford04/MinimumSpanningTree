0ecdb30439af746b5708e5346665fb07b9b0014a
AceCrawford04 <132942399+AceCrawford04@users.noreply.github.com>
2024-04-29 13:47:54 -0400

    Create Vertex.java

---
+++ Vertex.java
@@ -0,0 +1,20 @@
+//File: Vertex.java
+//Ace Crawford 
+//Fall 2023, Transy
+//PPL
+
+import java.io.*;
+
+public class Vertex{
+
+	private String vertexName;
+
+
+	public void setName(String name){
+		vertexName = name;
+	}
+
+	public String getName(){
+		return vertexName;
+	}
+}
